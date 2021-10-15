0449a70c28382559866876f7176e392ff0aab1f2
@vivanovskiy <vivanovskiy@smartdatatech.io>
2021-10-15 15:13:35 +0300

    forgot about Homework module :(

--- solution/index.js
+++ solution/index.js
@@ -11,13 +11,13 @@ module.exports = function(Homework) {
         const _asyncLengthPromise = promisified(asyncArray.length, );
         const _asyncLength = await _asyncLengthPromise;
         let i = 0;
-        let loopCondition = await promisified(less,i, _asyncLength);
+        let loopCondition = await promisified(Homework.less,i, _asyncLength);
         for (i; loopCondition;)
         {
             const arrayElement = await promisified(asyncArray.get, i);
             accumulator = await promisified(fn, accumulator, arrayElement, i, asyncArray);
-            i = await promisified(add, i, 1);
-            loopCondition = await promisified(less, i, _asyncLength);
+            i = await promisified(Homework.add, i, 1);
+            loopCondition = await promisified(Homework.less, i, _asyncLength);
         }
         cb(accumulator);
     }
